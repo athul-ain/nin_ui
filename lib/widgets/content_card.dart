130d44247b7d2c58cb202819cf9753e332036ee2
Athul A <athul@ain.one>
2025-02-26 17:22:03 +0530

    added ContentCard.animated

--- lib/widgets/content_card.dart
+++ lib/widgets/content_card.dart
@@ -10,6 +10,7 @@ class ContentCard extends StatelessWidget {
   final EdgeInsetsGeometry padding;
   final void Function()? onTap;
   final BorderRadiusGeometry? borderRadius;
+  final bool animated;
 
   const ContentCard({
     super.key,
@@ -20,12 +21,46 @@ class ContentCard extends StatelessWidget {
     this.padding = const EdgeInsets.all(0),
     this.onTap,
     this.borderRadius,
+    this.animated = false,
   });
+
+  const ContentCard.animated({
+    super.key,
+    required this.child,
+    this.color,
+    this.margin =
+        const EdgeInsets.only(left: 0.5, right: 0.5, top: 0, bottom: 5),
+    this.padding = const EdgeInsets.all(0),
+    this.onTap,
+    this.borderRadius,
+  }) : animated = true;
 
   @override
   Widget build(BuildContext context) {
     final cardColor = color ?? getContentColor(Theme.of(context).colorScheme);
     final cardTheme = Theme.of(context).cardTheme;
+
+    if (animated) {
+      return AnimatedContainer(
+        duration: Durations.medium3,
+        margin: margin,
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(
+          color: cardColor,
+          borderRadius: borderRadius ?? BorderRadius.circular(13),
+        ),
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onTap,
+            child: Padding(
+              padding: padding,
+              child: child,
+            ),
+          ),
+        ),
+      );
+    }
 
     return Card(
       margin: margin,
